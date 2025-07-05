7d6f68d877b395e9144b7eaf243db31080cfd859
Vazquez-Daniela <dv004282@gmail.com>
2025-07-04 18:15:08 -0600

    index

--- index.js
+++ index.js
@@ -3,23 +3,30 @@
  */
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+var express = require('express');
+var app = express(); //Contenedor de Endpoints o WS Restful
 
-const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
-const uri = "mongodb+srv://<usuario>:<contraseña>@cluster0.mongodb.net/?retryWrites=true&w=majority"; // cambia esto
-const client = new MongoClient(uri);
+const axios = require('axios');
+//Variables para conectar con MongoDB
+const { MongoClient } = require("mongodb");
+const mongoUri =  
+"mongodb+srv://dani:VNLlzT80Z16o1yi9@cluster0.114xfzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(mongoUri);
+const dbName = "unisport";
 let db;
 
+// Conectar a MongoDB
 async function conectarMongo() {
     try {
         await client.connect();
-        db = client.db("unisport"); // tu base de datos
         console.log("Conectado a MongoDB");
-    } catch (error) {
-        console.error("Error al conectar con MongoDB", error);
+        db = client.db(dbName);
+    } catch (err) {
+        console.error("Error conectando a MongoDB:", err);
     }
 }
 conectarMongo();
