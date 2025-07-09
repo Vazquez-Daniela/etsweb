8282ea4ad5a635bb566b02032f330d65c2908da9
Vazquez-Daniela <dv004282@gmail.com>
2025-07-08 18:03:25 -0600

    validarP

--- public/js/validarP.js
+++ public/js/validarP.js
@@ -8,19 +8,19 @@
 
     // Validaciones
     if (!nombre || !precio || !cantidad || !descripcion || !proveedor || !categoria) {
-        alert('⚠️ Todos los campos son obligatorios.');
+        alert('Todos los campos son obligatorios.');
         e.preventDefault();
         return;
     }
 
     if (isNaN(precio) || parseFloat(precio) <= 0) {
-        alert('⚠️ El precio debe ser un número positivo.');
+        alert('El precio debe ser un número positivo.');
         e.preventDefault();
         return;
     }
 
     if (!Number.isInteger(Number(cantidad)) || parseInt(cantidad) < 1) {
-        alert('⚠️ La cantidad debe ser un número entero mayor a 0.');
+        alert('La cantidad debe ser un número entero mayor a 0.');
         e.preventDefault();
         return;
     }
