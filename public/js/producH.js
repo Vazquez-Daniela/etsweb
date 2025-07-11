0b49066fe1181756418bb6ac14b5a8428408508f
Vazquez-Daniela <dv004282@gmail.com>
2025-07-11 11:37:25 -0600

    produc Hombre

---
+++ public/js/producH.js
@@ -0,0 +1,23 @@
+  fetch('/productos?categoria=Hombre')
+    .then(res => res.json())
+    .then(productos => {
+      const contenedor = document.getElementById('contenedor-productos');
+      contenedor.innerHTML = '';
+
+      productos.forEach(producto => {
+        const div = document.createElement('div');
+        div.className = 'item';
+
+        div.innerHTML = `
+          <span class="titulo-item">${producto.nombre}</span>
+          <img src="${producto.imagen}" alt="" class="img-item">
+          <span class="precio-item">$${producto.precio}</span>
+          <button class="boton-item">Agregar al Carrito</button>
+        `;
+
+        contenedor.appendChild(div);
+      });
+    })
+    .catch(err => {
+      console.error('Error cargando productos:', err);
+    });
